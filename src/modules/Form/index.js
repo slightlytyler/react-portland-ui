e77ca02014f863ef70ecbfbc37d0c0094241e632
Tyler Martinez <slightlytyler@gmail.com>
2016-05-20 12:36:31 -0500

    Basic validation working

--- src/modules/Form/index.js
+++ src/modules/Form/index.js
@@ -1,15 +1,37 @@
 import React, { Component, PropTypes } from 'react';
+import Formal from 'react-formal';
 
 export default class Form extends Component {
   static propTypes = {
     children: PropTypes.node,
+    schema: PropTypes.object.isRequired,
+    defaultValue: PropTypes.object.isRequired,
   };
+
+  state = {
+    errors: {},
+  };
+
+  getErrorForName = name => this.state.errors[name];
+
+  handleErrors = errors => this.setState({ errors });
+
+  renderChildren() {
+    return React.Children.map(this.props.children, child => (
+      React.cloneElement(child, { error: this.getErrorForName(child.props.name) })
+    ));
+  }
 
   render() {
     return (
-      <form className="pui--form">
-        {this.props.children}
-      </form>
+      <Formal
+        className="pui--form"
+        schema={this.props.schema}
+        defaultValue={this.props.defaultValue}
+        onError={this.handleErrors}
+      >
+        {this.renderChildren()}
+      </Formal>
     );
   }
 }
--- src/modules/Input/index.js
+++ src/modules/Input/index.js
@@ -1,13 +1,16 @@
 import React, { Component, PropTypes } from 'react';
+import { Field, Message } from 'react-formal';
 import classnames from 'classnames';
 
 export default class Input extends Component {
   static propTypes = {
+    name: PropTypes.string,
     label: PropTypes.string,
     placeholder: PropTypes.oneOfType([
       PropTypes.string,
       PropTypes.bool,
     ]),
+    error: PropTypes.array,
     responsive: PropTypes.bool,
   };
 
@@ -29,19 +32,28 @@ export default class Input extends Component {
 
   renderLabel() {
     if (this.props.label) {
-      return <label className="label">{this.props.label}</label>;
+      return (
+        <div className="label">
+          <label className="body">{this.props.label}</label>
+          <Message className="message" for={this.props.name} />
+        </div>
+      );
     }
 
     return undefined;
   }
 
   render() {
-    const classes = classnames('pui--input', { responsive: this.props.responsive });
+    const classes = classnames(
+      'pui--input',
+      { error: this.props.error },
+      { responsive: this.props.responsive }
+    );
 
     return (
       <div className={classes}>
         {this.renderLabel()}
-        <input className="input" placeholder={this.placeholder()} />
+        <Field className="input" name={this.props.name} placeholder={this.placeholder()} />
       </div>
     );
   }
