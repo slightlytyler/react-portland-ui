9e6bf0b86bdf15d0e6d0d4050686bb48508a2b57
Tyler Martinez <slightlytyler@gmail.com>
2016-06-19 22:28:12 -0500

    Better styles for props documentation. resolves #6

--- docs/src/components/Documentation/PropTypes/index.js
+++ docs/src/components/Documentation/PropTypes/index.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import classnames from 'classnames';
 import { capitalize } from 'lodash';
 import {
   Table,
@@ -17,28 +18,51 @@ export default class PropTypesDocumentation extends Component {
   columns = [
     { name: 'name', selector: key => key },
     { name: 'type', selector: (key, value) => value.type.name },
-    { name: 'required', selector: (key, value) => (value.required ? 'true' : 'false') },
     {
       name: 'default',
       selector: (key, value) => (
-        value.defaultValue ? value.defaultValue.value : 'undefined'
+        value.defaultValue ? value.defaultValue.computed : undefined
       ),
     },
     { name: 'description', selector: (key, value) => value.description },
   ];
 
   renderColumns = () => this.columns.map(column => (
-    <TableHeadCell key={column.name}>{capitalize(column.name)}</TableHeadCell>
+    <TableHeadCell className="cell" key={column.name}>{capitalize(column.name)}</TableHeadCell>
   ));
 
+  renderCell = (value, columnName, prop) => {
+    switch (columnName) {
+      case 'name':
+      case 'type':
+        return <code>{value}</code>;
+
+      case 'required':
+        return value ? <code>required</code> : '';
+
+      case 'default': {
+        if (prop.required) return <span className="required">Required</span>;
+
+        if (typeof value === 'string') return value;
+
+        return typeof value === 'undefined'
+          ? <code>undefined</code>
+          : <code>{value.toString()}</code>;
+      }
+
+      default:
+        return value;
+    }
+  };
+
   renderCells = (key, value) => this.columns.map(column => (
-    <TableCell key={`${key}-${column.name}`}>
-      {column.selector(key, value)}
+    <TableCell className={classnames('cell', column.name)} key={`${key}-${column.name}`}>
+      {this.renderCell(column.selector(key, value), column.name, value)}
     </TableCell>
   ));
 
   renderRows = () => Object.keys(this.props.propTypes).map(key => (
-    <TableRow key={key}>
+    <TableRow className="row" key={key}>
       {this.renderCells(key, this.props.propTypes[key])}
     </TableRow>
   ));
