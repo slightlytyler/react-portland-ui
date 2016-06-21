da119975d5f247b780b41cc42e8038feb7918dcf
Tyler Martinez <slightlytyler@gmail.com>
2016-06-20 23:51:22 -0500

    Expandable code examples. resolves #7

--- docs/src/components/Documentation/Examples/Item/index.js
+++ docs/src/components/Documentation/Examples/Item/index.js
@@ -1,21 +1,49 @@
 import React, { Component, PropTypes } from 'react';
 import reactElementToJSXString from 'react-element-to-jsx-string';
-import { CodeBlock } from 'pui';
+import classnames from 'classnames';
+import { CodeBlock, Button } from 'pui';
 
 export default class ExamplesDcumentationItem extends Component {
   static propTypes = {
     children: PropTypes.node,
   };
 
-  renderCode = () => reactElementToJSXString(this.props.children);
+  state = {
+    expanded: false,
+  };
+
+  toggleExpand = () => this.setState({ expanded: !this.state.expanded });
+
+  renderExpandButton = () => (
+    <Button ghost className="expand-button" onClick={this.toggleExpand}>
+      {this.state.expanded ? 'Hide Source' : 'Show Source'}
+    </Button>
+  );
+
+  renderExample = () => (
+    <section className="example">{this.props.children}</section>
+  );
+
+  renderCode = () => {
+    if (this.state.expanded) {
+      return (
+        <CodeBlock className="code" language="jsx">
+          {reactElementToJSXString(this.props.children)}
+        </CodeBlock>
+      );
+    }
+
+    return undefined;
+  };
 
   render() {
+    const classes = classnames('item', { expanded: this.state.expanded });
+
     return (
-      <div className="item">
-        <section className="example">{this.props.children}</section>
-        <CodeBlock className="code" language="jsx">
-          {this.renderCode()}
-        </CodeBlock>
+      <div className={classes}>
+        {this.renderExpandButton()}
+        {this.renderExample()}
+        {this.renderCode()}
       </div>
     );
   }
