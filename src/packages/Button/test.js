8cc9652f7e173bd2ea007c92517f402085f0950e
Tyler Martinez <slightlytyler@gmail.com>
2016-06-24 14:51:43 -0500

    tests use sinon-chai and 'should' syntax

--- src/packages/Button/test.js
+++ src/packages/Button/test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { expect } from 'chai';
 import { spy } from 'sinon';
 import { render, shallow } from 'enzyme';
 import Button from './index';
@@ -8,49 +7,49 @@ import { Button as FormalButton } from 'react-formal';
 describe('<Button />', () => {
   it('renders', () => {
     const wrapper = render(<Button />);
-    expect(wrapper).to.be.present();
+    wrapper.should.be.present();
   });
 
   it('render children', () => {
     const wrapper = shallow(<Button>Test</Button>);
-    expect(wrapper).to.have.text('Test');
+    wrapper.should.have.text('Test');
   });
 
   it('renders a form button when in form context', () => {
     const context = { reactFormalContext: true };
     const wrapper = shallow(<Button />, { context });
-    expect(wrapper.find(FormalButton)).to.have.length(1);
+    wrapper.find(FormalButton).should.have.length(1);
   });
 
   it('accepts a className', () => {
     const wrapper = shallow(<Button className="some-class" />);
-    expect(wrapper).to.have.className('some-class');
+    wrapper.should.have.className('some-class');
   });
 
   it('accepts a type', () => {
     const wrapper = shallow(<Button type="submit" />);
-    expect(wrapper).to.have.attr('type', 'submit');
+    wrapper.should.have.attr('type', 'submit');
   });
 
   it('accepts an onClick action', () => {
     const onClick = spy();
     const wrapper = shallow(<Button onClick={onClick} />);
     wrapper.simulate('click');
-    expect(onClick.calledOnce).to.equal(true);
+    onClick.should.have.been.calledOnce;
   });
 
   it('accepts a ghost prop', () => {
     const wrapper = shallow(<Button ghost />);
-    expect(wrapper).to.have.className('ghost');
+    wrapper.should.have.className('ghost');
   });
 
   it('accepts a fluid prop', () => {
     const wrapper = shallow(<Button fluid />);
-    expect(wrapper).to.have.className('fluid');
+    wrapper.should.have.className('fluid');
   });
 
   it('accepts a big prop', () => {
     const wrapper = shallow(<Button big />);
-    expect(wrapper).to.have.className('big');
+    wrapper.should.have.className('big');
   });
 });
--- tests.js
+++ tests.js
@@ -1,6 +1,9 @@
 const chai = require('chai');
+const sinonChai = require('sinon-chai');
 const chaiEnzyme = require('chai-enzyme');
 
+chai.should();
+chai.use(sinonChai);
 chai.use(chaiEnzyme());
 
 // concatenates all tests and uses webpack to transpile into ES5, (cannot merge into karma.conf.js)
